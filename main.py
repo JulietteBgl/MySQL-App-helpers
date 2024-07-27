a58b4d96cd5a959a07f008e9fb5ff60b76a4de76
Juliette Bassnagel <j.bassnagel@gmail.com>
2024-07-27 18:28:20 +0200

    STY: clean (remove) logs

--- main.py
+++ main.py
@@ -13,16 +13,20 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)-5s [%(module)s - %(
 
 def main(connection, files):
     if args.clean_table:
+        logging.info('Deleting all rows in the table.')
         delete_rows(connection)
+
     nb_rows_inserted = 0
     for file_path in files:
         with open(f'applications/{file_path}', 'r') as f:
             log = f.read()
         formatted_log = format_log(log)
+
         if is_expected_format(formatted_log):
             nb_rows_inserted += insert_logs(connection, formatted_log)
         else:
             logging.error(f"log {formatted_log} doesn't follow the expected format and can't be added to the database")
+
     return nb_rows_inserted
 
 
@@ -31,16 +35,13 @@ if __name__ == "__main__":
 
     try:
         db_connection = connect_to_database(DATABASE_NAME)
-        if db_connection.is_connected():
-            logging.info('MYSQL connection established.')
-        else:
+        if not db_connection.is_connected():
             logging.error('Failed to connect to MYSQL.')
-            exit(1)  # RAISE ERROR
+            exit(1)
     except Exception as e:
         logging.error(f"Exception occurred while connecting to the database: {e}")
         exit(1)
 
-    logging.info('Reading logs...')
     json_files = [pos_json for pos_json in os.listdir(LOGS_FOLDER_NAME) if pos_json.endswith('.json')]
     logging.info('Starting main...')
     try:
@@ -49,9 +50,7 @@ if __name__ == "__main__":
     except Exception as e:
         logging.error(f"Exception occurred in main function: {e}")
     finally:
-        logging.info('Closing MYSQL connection...')
         if db_connection.is_connected():
+            logging.info('Closing MYSQL connection...')
             db_connection.close()
             logging.info('MYSQL connection closed.')
-        else:
-            logging.error('MYSQL connection was already closed.')
