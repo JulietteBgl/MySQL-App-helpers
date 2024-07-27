f7ad48109b981e95ac629deb5971142733b891b9
Juliette Bassnagel <j.bassnagel@gmail.com>
2024-07-27 18:05:35 +0200

    REF: introduce variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+DATABASE_NAME = 'tests_juliette'
+LOGS_FOLDER_NAME = 'applications'
+TABLE_NAME = 'application'
+
+# database columns
+ID = 'id'
+THERAPEUTIC_AREA = 'therapeutic_area'
+CREATED_AT = 'created_at'
+SITE_NAME = 'site_name'
+SITE_CATEGORY = 'site_category'
